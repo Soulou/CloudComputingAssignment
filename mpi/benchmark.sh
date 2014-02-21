fd8bd4d5e2c62cf135f65d3b973290a950e93979
Leo Unbekandt <leo@unbekandt.eu>
2014-02-21 16:50:00 +0000

    Only 2 matrix of each size

--- mpi/benchmark.sh
+++ mpi/benchmark.sh
@@ -4,9 +4,9 @@ nb_nodes=$(cat hosts | wc -l)
 mkdir -p results
 
 for n in 128 256 512 1024 ; do
-  ./matrix_gen.rb ${n} ${n} > matrix1_${nb_nodes}_${n}
-  ./matrix_gen.rb ${n} ${n} > matrix2_${nb_nodes}_${n}
+  [ -e "matrix1_$n" ] || ./matrix_gen.rb $n $n > matrix1_$n
+  [ -e "matrix2_$n" ] || ./matrix_gen.rb $n $n > matrix2_$n
 
-  ./run_matrix_multiply.sh hosts matrix1_${nb_nodes}_${n} matrix2_${nb_nodes}_${n} container result_${nb_nodes}_${n}
+  ./run_matrix_multiply.sh hosts matrix1_$n matrix2_$n container result_${nb_nodes}_${n}
   mv result_${nb_nodes}_${n} results
 done
